16b28f5e1299c262df641cfeeae57341cc6a304b
Alexis <alexis.pele@pm.me>
2026-03-24 18:16:07 +0100

    Default dark mode setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark } from '@vueuse/core';
+
+useDark({ storageKey: "theme" })
+</script>
 
 <template>
   <RouterView />
 </template>
-
-<style scoped></style>
